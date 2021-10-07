04ce1036a9ccd229b6ce9ce605f3570d0c97ef8b
Francisco <franciscogajardo.perez@gmail.com>
2021-10-06 23:08:11 -0300

    Consumiendo una api con fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect,useState} from 'react'
+
 
 function App() {
+  const url = 'https://jsonplaceholder.typicode.com/posts'
+
+  const [todos , setTodos] = useState()
+
+  const fetchApi = async () => {
+    const response = await fetch(url)
+    const responseJSON = await response.json();
+    setTodos(responseJSON)
+
+  }
+
+  useEffect(() =>{
+    fetchApi()
+  }, [])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Taks</h1> 
+      <table className="table table-striped" >
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Body</th>
+          </tr>
+        </thead>
+        {!todos ? 'Cargando...': 
+            todos.map((todo,index) =>{
+            return  <tbody key={index}>
+              <tr >
+                <th scope="row" >{todo.id}</th>
+                <td>{todo.title}</td>
+                <td>{todo.body}</td>
+              </tr>
+            </tbody>
+            })
+            }
+      </table>
+     
     </div>
   );
 }
